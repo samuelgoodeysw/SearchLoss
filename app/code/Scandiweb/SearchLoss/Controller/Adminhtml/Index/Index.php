cc3b27a436fbef0d62807d971c7abf629ec84b2b
Sam <samuel.goodey@scandiweb.com>
2026-04-28 18:35:01 +0200

    Rename Search Loss admin page title to audit

--- app/code/Scandiweb/SearchLoss/Controller/Adminhtml/Index/Index.php
+++ app/code/Scandiweb/SearchLoss/Controller/Adminhtml/Index/Index.php
@@ -24,7 +24,7 @@ class Index extends Action
     {
         $resultPage = $this->resultPageFactory->create();
         $resultPage->setActiveMenu('Scandiweb_SearchLoss::searchloss');
-        $resultPage->getConfig()->getTitle()->prepend(__('Search Loss'));
+        $resultPage->getConfig()->getTitle()->prepend(__('Search Loss Audit'));
 
         return $resultPage;
     }
